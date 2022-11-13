d2c8afc94643377e398977bf1485d2a72a5c6b17
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-11-13 09:27:45 +0900

    added install-homebrew.sh
    Signed-off-by: KOSHIKAWA Kenichi <reishoku.misc@pm.me>

---
+++ scripts/install-homebrew.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export NONINTERACTIVE=1
+
+# system check
+[[ "$(uname -s)" != "Linux" ]] && echo "This system is not Linux apparently."; exit 1;
+[[ -f "/etc/os-release" ]] && source /etc/os-release
+[[ "$ID_LIKE" == "*debian*" ]] && \
+(
+  command -v sudo && \
+    sudo apt-get install -y \
+      --no-install-recommends --no-install-suggests \
+      build-essential file curl git
+)
+[[ "$ID_LIKE" == "*fedora*" ]] && \
+(
+  # yum is deprecated; using dnf instead.
+  command -v dnf && \
+    sudo dnf install -y \
+      "Development Tools" "Development Libraries" curl file git
+)
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
